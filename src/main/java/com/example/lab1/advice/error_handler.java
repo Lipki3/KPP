aa104f870aa918d8cb455cc2ac1a52b2081842f5
Lipki3 <73040453+Lipki3@users.noreply.github.com>
2022-03-28 01:53:30 +0300

    Add files via upload

--- src/main/java/com/example/lab1/advice/error_handler.java
+++ src/main/java/com/example/lab1/advice/error_handler.java
@@ -1,7 +1,7 @@
 package com.example.lab1.advice;
 
+
 import com.example.lab1.exception.CustomException;
-import com.example.lab1.Response.response;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +13,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class error_handler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<response> handleException(@NotNull CustomException e) {
+    public ResponseEntity<Object> handleException(@NotNull CustomException e) {
         logger.error("ERROR CODE 400", e);
-        return new ResponseEntity<>(new response(e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<response> handleException(@NotNull Exception e) {
+    public ResponseEntity<Object> handleException(@NotNull Exception e) {
         logger.error("ERROR CODE 500", e);
-        return new ResponseEntity<>(new response(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
