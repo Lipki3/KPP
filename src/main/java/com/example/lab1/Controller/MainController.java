3a545fbcdccf30ae3de4a78223a1b6762e5b1488
Lipki3 <73040453+Lipki3@users.noreply.github.com>
2022-05-28 10:00:39 +0300

    Add files via upload

--- src/main/java/com/example/lab1/Controller/MainController.java
+++ src/main/java/com/example/lab1/Controller/MainController.java
@@ -9,7 +9,6 @@ import com.example.lab1.App;
 import com.example.lab1.Results.Solution;
 import com.example.lab1.Validations.InputValidation;
 import org.apache.logging.log4j.Level;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +20,8 @@ import javax.validation.Valid;
 import com.example.lab1.Stats.Statistics;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -69,23 +68,31 @@ public class MainController {
 
 
     private static final String template = "\nDate: %d.%d.%d, Day: %s";
+    private static final String template1 = "\nDate: %d.%d.%d, Date is incorrect";
 
     @PostMapping("/stats")
     public ResponseEntity<?> EnterStream(@Valid @RequestBody List<DataClass> bodyList){
         Statistics st = new Statistics();
         List<String> res_output = new ArrayList<>();
         NumberOfRequests.IncremetNumber();
-        int[] days = {0,0,0,0,0,0,0}; String popular = ""; int max = 0;
+        int[] days = {0,0,0,0,0,0,0}; String popular = ""; int max = 0; AtomicInteger incorrectdate = new AtomicInteger();
          //res_output.add(String.format(template, st.findJanuary(bodyList), st.findDecember(bodyList)));
         bodyList.forEach((element)->{
-            res_output.add(String.format(template, element.getDate(), element.getMonth(), element.getYear(), st.Day(element)));
-            if (st.Day(element) == "Monday") days[0]++;
-            if (st.Day(element) == "Thursday") days[1]++;
-            if (st.Day(element) == "Wednesday") days[2]++;
-            if (st.Day(element) == "Thursday") days[3]++;
-            if (st.Day(element) == "Friday") days[4]++;
-            if (st.Day(element) == "Saturday") days[5]++;
-            if (st.Day(element) == "Sunday") days[6]++;
+            if (element.getYear() < 1900 || element.getYear() > 2100 || element.getMonth()>12 ||
+                    element.getMonth() <1 || element.getDate()>31 || element.getDate()<1) {
+                res_output.add(String.format(template1, element.getDate(), element.getMonth(), element.getYear()));
+                incorrectdate.getAndIncrement();
+            }
+            else {
+                res_output.add(String.format(template, element.getDate(), element.getMonth(), element.getYear(), st.Day(element)));
+                if (st.Day(element) == "Monday") days[0]++;
+                if (st.Day(element) == "Thursday") days[1]++;
+                if (st.Day(element) == "Wednesday") days[2]++;
+                if (st.Day(element) == "Thursday") days[3]++;
+                if (st.Day(element) == "Friday") days[4]++;
+                if (st.Day(element) == "Saturday") days[5]++;
+                if (st.Day(element) == "Sunday") days[6]++;
+            }
         });
         max = 0;
         for (int i = 0; i<6; i++){
@@ -101,8 +108,10 @@ public class MainController {
         if (max == 6) popular = "Sunday";
 
         long size = st.calcSize(bodyList);
+        int correctdate = Math.toIntExact(size - incorrectdate.get());
 
-        return new ResponseEntity<>("Size:" + size +"\n"+ res_output + "\nMost popular: " + popular, HttpStatus.OK);
+        return new ResponseEntity<>("Size:" + size + "\nCorrect values: " +  correctdate + "\nIncorrect values: "
+                + incorrectdate.get() + res_output + "\nMost popular: " + popular, HttpStatus.OK);
     }
 
 }
